ac3b72238cec6e7673929f49c11c4fd25d895118
Jacob Barber <jacob.barber@disneystreaming.com>
2023-09-28 16:46:41 -0500

    uncomment stuff, fix steam path

--- docker/run.sh
+++ docker/run.sh
@@ -6,42 +6,42 @@
 USERNAME="$1"
 CMD_STRING="$2"
 
-STEAM_DIR="$HOME/.local/share/Steam"
+STEAM_DIR="$HOME/Steam"
 STEAM_APPS_DIR="$STEAM_DIR/steamapps"
 CHIVALRY2_DIR="$STEAM_APPS_DIR/common/Chivalry 2"
-PROTON_DIR="$STEAM_APPS_DIR/common/Proton 8.x"
+PROTON_DIR="$STEAM_APPS_DIR/common/Proton 8.0"
 
 export STEAM_COMPAT_DATA_PATH=~/.local/share/Steam/steamapps/compatdata
 export STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.local/share/Steam/
 
 # Install Chivalry 2 if not installed
-if [ ! -d $CHIVALRY2_DIR ]; then
-  echo "Installing Proton 8.x"
+if [ ! -d "$CHIVALRY2_DIR" ]; then
+  echo "Installing Proton 8.x to '$PROTON_DIR'"
   steamcmd \
     +login "$USERNAME" \
     +app_update 2348590 \
     +quit
 
-  #echo "Installing Chivalry 2 to '/opt/Chivalry 2'..."
-  #steamcmd \
-  #  +@sSteamCmdForcePlatformType windows \
-  #  +login "$USERNAME" \
-  #  +app_update 1824220 \
-  #  +quit
+  echo "Installing Chivalry 2 to '$CHIVALRY2_DIR'..."
+  steamcmd \
+    +@sSteamCmdForcePlatformType windows \
+    +login "$USERNAME" \
+    +app_update 1824220 \
+    +quit
 
-  #echo "Installing C2UMP Launcher"
-  #wget -o "$CHIVALRY2_DIR/Chivalry2Launcher.exe" 'https://github.com/C2UMP/C2Loader/releases/latest/download/Chivalry2Launcher.exe'
+  echo "Installing C2UMP Launcher"
+  wget -o "$CHIVALRY2_DIR/Chivalry2Launcher.exe" 'https://github.com/C2UMP/C2Loader/releases/latest/download/Chivalry2Launcher.exe'
 
-  #echo "Installing C2UMP Plugin Loader"
-  #wget -o "$CHIVALRY2_DIR/TBL/Binaries/Win64/XAPOFX1_5.dll" 'https://github.com/C2UMP/C2PluginLoader/releases/latest/download/XAPOFX1_5.dll'
+  echo "Installing C2UMP Plugin Loader"
+  wget -o "$CHIVALRY2_DIR/TBL/Binaries/Win64/XAPOFX1_5.dll" 'https://github.com/C2UMP/C2PluginLoader/releases/latest/download/XAPOFX1_5.dll'
 
-  #echo "Installing Unchained Plugin"
-  #mkdir "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins"
-  #wget -o "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins/UnchainedPlugin.dll" 'https://github.com/Chiv2-Community/UnchainedPlugin/releases/latest/download/UnchainedPlugin.dll'
+  echo "Installing Unchained Plugin"
+  mkdir "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins"
+  wget -o "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins/UnchainedPlugin.dll" 'https://github.com/Chiv2-Community/UnchainedPlugin/releases/latest/download/UnchainedPlugin.dll'
   
   # Can't be arsed to figure out the correct plugins directory, so just copy it to both
-  #mkdir "$CHIVALRY2_DIR/Plugins"
-  #cp "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins/UnchainedPlugin.dll" "$CHIVALRY2_DIR/Plugins/UnchainedPlugin.dll"
+  mkdir "$CHIVALRY2_DIR/Plugins"
+  cp "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins/UnchainedPlugin.dll" "$CHIVALRY2_DIR/Plugins/UnchainedPlugin.dll"
 
 
   echo "Installed files"
@@ -60,6 +60,6 @@ tree '/config'
 
 rsync -r '/config' "$CHIVALRY2_DIR"
 
-$PROTON_DIR/proton $CHIVALRY2_DIR/Chivalry2Launcher.exe "$CMD_STRING" -nullrhi -rcon
+"$PROTON_DIR/proton" "$CHIVALRY2_DIR/Chivalry2Launcher.exe" "$CMD_STRING" -nullrhi -rcon
 
 sleep 10000
