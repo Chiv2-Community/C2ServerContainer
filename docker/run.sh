4f345dc450c31ad7d504b6a2ff844ee077516bfb
Jacob Barber <jacoby6000@gmail.com>
2023-09-28 23:34:41 -0500

    updates

--- docker/run.sh
+++ docker/run.sh
@@ -6,6 +6,8 @@
 USERNAME="$1"
 CMD_STRING="$2"
 
+export PATH=$PATH:/home/steam/steamcmd
+
 PROTON_8_X_APP_ID=2348590 
 CHIVALRY2_APP_ID=1824220
 
@@ -22,13 +24,13 @@ export STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.local/share/Steam/
 # Install Chivalry 2 if not installed
 if [ ! -d "$CHIVALRY2_DIR" ]; then
   echo "Installing Proton 8.x to '$PROTON_DIR'"
-  steamcmd \
+  steamcmd.sh \
     +login "$USERNAME" \
     +app_update $PROTON_8_X_APP_ID \
     +quit
 
   echo "Installing Chivalry 2 to '$CHIVALRY2_DIR'..."
-  steamcmd \
+  steamcmd.sh \
     +@sSteamCmdForcePlatformType windows \
     +login "$USERNAME" \
     +app_update $CHIVALRY2_APP_ID \
@@ -47,12 +49,11 @@ if [ ! -d "$CHIVALRY2_DIR" ]; then
   # Can't be arsed to figure out the correct plugins directory, so just copy it to both
   mkdir "$CHIVALRY2_DIR/Plugins"
   cp "$CHIVALRY2_DIR/TBL/Binaries/Win64/Plugins/UnchainedPlugin.dll" "$CHIVALRY2_DIR/Plugins/UnchainedPlugin.dll"
-
-  echo "Installed files"
-  tree "$STEAM_APPS_DIR"
 fi
 
-echo "Found $(ls -1 '/mods' | wc -l) mods in /mods"
+mkdir -p "$STEAM_COMPAT_DATA_PATH"
+mkdir -p "$CONFIG_DIR"
+
 tree '/mods'
 echo ""
 echo "Copying mods from /mods to $CHIVALRY2_DIR/TBL/Content/Paks"
@@ -64,6 +65,4 @@ tree '/config'
 
 rsync -r '/config/*.ini' "$CONFIG_DIR/"
 
-"$PROTON_DIR/proton" "$CHIVALRY2_DIR/Chivalry2Launcher.exe" "$CMD_STRING" -nullrhi -rcon
-
-sleep 10000
+"$PROTON_DIR/proton" run "$CHIVALRY2_DIR/Chivalry2Launcher.exe" $CMD_STRING -nullrhi -rcon
--- example/run.sh
+++ example/run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-MOD_DIR=$1
-CONFIG_DIR=$2
-USERNAME=$3
-CMD_STRING=$4
+MOD_DIR="$1"
+CONFIG_DIR="$2"
+USERNAME="$3"
+CMD_STRING="$4"
 
 # docker pull jacoby6000/C2ServerContainer:latest
+STEAM_DIR="/home/root/.steam/steam"
+STEAM_APPS_DIR="$STEAM_DIR/steamapps"
+
+docker volume create steamcmd_login_volume
+docker volume create steamcmd_volume
 
 docker run \
   -it \
@@ -16,4 +21,6 @@ docker run \
   -p 7777:7777/udp \
   -v "$MOD_DIR":/mods \
   -v "$CONFIG_DIR":/config \
+  -v "steamcmd_login_volume:/home/steam/Steam" \
+  -v "steamcmd_volume:/home/steam/steamcmd" \
   jacoby6000/chiv2-container:latest $USERNAME $CMD_STRING
